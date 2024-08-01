2e44fdc0ee95eb6c539d029edc1291ad7762d8c7
Elry <gh@elry.moe>
2024-08-01 20:57:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,6 @@ scrDir=$(dirname "$(realpath "$0")")
 
 "${scrDir}/Scripts/install_hyprdots.sh"
 
-cp "${scrDir}/monitors.conf" "${scrDir}/Dotfiles/hypr"
-
 cp -rf "${scrDir}/Pictures" "$HOME"
 
 cp -rf "${scrDir}/Dotfiles/hypr" "$HOME/.config/"
